0ede5abd60ac66f092c8b8caabba0148c962116c
PavelMaltsev20 <PavelMaltsev20maltsevpavel95@gmail.com>
2020-11-04 15:15:59 +0200

    !

--- app/src/main/java/com/example/bugonscreenkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bugonscreenkotlin/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.view.WindowManager
-import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
